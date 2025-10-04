566c98b9b9913e3b445e81c3a89b63349171e686
bruvie-fr <dahaloliver83@gmail.com>
2025-10-04 14:25:27 -0400

    Asteroids move now... I forgot a fucking paramater took me 20 mins to find the error. pygame is ass

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
+
 import pygame
 from constants import *
-from player import *
-
-
+from player import Player
+from asteroidfield import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    asteroidfield = AsteroidField()
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return False
-        player.update(dt)
+                return
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        dt = clock.tick(60) / 100
 
-
-
-    
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
